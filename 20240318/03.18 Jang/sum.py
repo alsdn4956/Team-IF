964a32f1c72af636126fc24985ca790a64d73fcf
MinwooJang <46252059+alsdn4956@users.noreply.github.com>
2024-03-18 20:14:59 +0900

    Create sum.py

---
+++ 20240318/03.18 Jang/sum.py
@@ -0,0 +1,15 @@
+def sum_range(begin, end, step = 1):
+    sum=0    #지역변수
+    
+    for n in range(begin, end, step):
+        sum+= n
+    return sum
+
+if __name__ == "__main__":
+    print('sum=', sum_range(1,10))
+    print('sum=', sum_range(1,10,2))
+    print('sum=', sum_range(step=3, begin=1, end=10))
+    
+    
+    
+    
